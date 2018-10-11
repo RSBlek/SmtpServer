b37091d9182eac88dcade0fd59f61975750bf771
chixlol <38309568+chixlol@users.noreply.github.com>
2018-10-11 22:39:42 +0200

    Initial upload

---
+++ Connection/SmtpConnectionHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Text;
+using TCPServer;
+
+namespace SMTPServer
+{
+    internal class SmtpConnectionHandler
+    {
+        private ConcurrentDictionary<TcpConnection, SmtpConnection> smtpConnections = new ConcurrentDictionary<TcpConnection, SmtpConnection>();
+
+        internal void AddSmtpConnection(TcpConnection tcpConnection, SmtpConnection smtpConnection)
+        {
+            smtpConnections.TryAdd(tcpConnection, smtpConnection);
+        }
+
+        internal SmtpConnection GetSmtpConnection(TcpConnection tcpConnection)
+        {
+            return smtpConnections[tcpConnection];
+        }
+
+    }
+}
